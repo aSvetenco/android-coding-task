3a890deaa0d34b21c50683fdb3fb15e9012eed32
Alex Svetenco <asvetenco@gmail.com>
2020-10-13 10:26:40 +0300

    --Add validator test (#12)
    [Ticket: Prepare to release]

---
+++ app/src/test/java/com/sa/betvictor/ui/TweetQueryValidatorTest.kt
@@ -0,0 +1,33 @@
+package com.sa.betvictor.ui
+
+import junit.framework.TestCase.assertFalse
+import junit.framework.TestCase.assertTrue
+import org.junit.Test
+
+class TweetQueryValidatorTest {
+
+    private val validator = TweetQueryValidator()
+
+    @Test
+    fun `isValid return false if query is empty`() {
+        val query = ""
+
+        assertFalse(validator.isValid(query))
+    }
+
+    @Test
+    fun `isValid return false if query is longer then limit (128 symbols)`() {
+        val query =
+            "12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890"
+
+        assertFalse(validator.isValid(query))
+    }
+
+    @Test
+    fun `isValid return true if query is not empty and is shorter then limit (128 symbols)`() {
+        val query = "Hello"
+
+        assertTrue(validator.isValid(query))
+    }
+}
+
